40ae45e28e92a05259009dd411faafe66e8b3205
Scatterlab <scatterlab@scatterlabs-iMac.local>
2026-02-17 11:31:20 -0800

    First push to new repository where we will keep the version control for ScatterAnalysis script

--- ScatterAnalysis.m
+++ ScatterAnalysis.m
@@ -41,6 +41,8 @@ clear; close all; clc;
 
 %% FILE AND EXPERIMENT TYPE
 
+% Mock data files for debugging
+
 % String path to the folder will all the data
 folder.data_path = '/Users/scatterlab/CSU Fullerton Dropbox/Scatter Lab/Shared/GWPAC_Lab_Data/ARS_TRS/2024_7_12_11_20_27_Crystaline_Silicon_SLED_ARS';
 
